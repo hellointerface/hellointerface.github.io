0cce01b1cc2937eb856671c0642c0194b531bacd
hellointerface <thrive@interfaceschool.com>
2016-11-19 16:52:15 -0600

    Update script.js

--- nov19/shonna/js/script.js
+++ nov19/shonna/js/script.js
@@ -1,9 +1,10 @@
 $(document).ready(function() {
-  $("h1, p").fadeIn(5000);
+  $("h1, p").fadeIn("fast");
+  $("h1").slideDown();
   $("button").click(function(){
     $("img").toggle();
   });
   $("body").click(function() {
-    $("body").css("background-color", "purple");
+    $("body").css("background-color", "yellow");
   });
 });
